2d63c33f9628527e1c6dbc2de1f43ca7d5615cf7
amin1918 <221761407+amin1918@users.noreply.github.com>
2025-08-29 01:18:10 +0300

    Update services.js
    user axios and baseURL

--- src/services/services.js
+++ src/services/services.js
@@ -1,32 +1,28 @@
+import axios from "axios";
 
+// Axios Client
+const client = axios.create({
+  baseURL: "https://fakestoreapi.com",
+});
 
+// Get all products
 export async function getProducts() {
   try {
-    const result = await fetch("https://fakestoreapi.com/products"); // ✅ آدرس کامل
-
-    if (!result.ok) {
-      throw new Error("Server doesn't respond");
-    }
-
-    const products = await result.json();
-    return products;
+    const { data } = await client.get("/products");
+    return data;
   } catch (error) {
-    console.log("Error:", error);
+    console.error("Error fetching products:", error);
     return [];
   }
 }
 
+// Get single product by ID
 export async function getProduct(id) {
   try {
-    const result = await fetch(`https://fakestoreapi.com/products
-/${id}`)
-    if (!result.ok) {
-      throw new Error("Server doesn't respond")
-    }
-    const product = await result.json()
-    return product
+    const { data } = await client.get(`/products/${id}`);
+    return data;
   } catch (error) {
-    console.log("Error:", error);
+    console.error("Error fetching product:", error);
     return null;
   }
 }
